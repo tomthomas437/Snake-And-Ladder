36e0c936ec8e24841f1ec89ffe37b6eb2f63b8a4
tomthomas437@gmail.com <tomthomas437@gmail.com>
2021-11-14 19:11:54 +0530

    Player at position 0

---
+++ src/SnakeAndLadder.java
@@ -0,0 +1,12 @@
+
+public class SnakeAndLadder {
+	public static void main(String[] args) {
+		System.out.println(" Welcome to the Snake And Ladder Game");
+		
+		int player_pos = 0;
+		
+		System.out.println("Player position is: " + player_pos);
+		
+	}
+
+}
